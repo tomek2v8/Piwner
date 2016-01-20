e9b7efd00b230e061f924cf99d344acf042bb86f
piwner <tomek2v8@gmail.com>
2016-01-20 13:37:29 +0100

    2 ver
    like wtf, biały ekran

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('pageCtrl', function($scope) {
+
+})
+   
+.controller('page2Ctrl', function($scope) {
+
+})
+   
+.controller('page3Ctrl', function($scope) {
+
+})
+ 
